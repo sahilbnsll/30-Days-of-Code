d6ff9e33749560e0eacae74b83055a3ba22127bb
Sahil Bansal <bansalsahil740@gmail.com>
2021-03-17 22:32:43 +0530

    Problem 2-DataTypes @30daysofcode

---
+++ Day01.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+	
+    public static void main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+		
+        Scanner scan = new Scanner(System.in);
+        int i2;
+        double d2;
+        String s2;
+        i2 = scan.nextInt();
+        d2 = scan.nextDouble();
+
+        scan.nextLine();
+        s2 = scan.nextLine();
+        System.out.println(i + i2);
+        System.out.println(d + d2);
+        System.out.println(s.concat(s2));
+
+        scan.close();
+    }
+}
