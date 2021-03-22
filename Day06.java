e97e33771b34346af409bb724662fff58695d72e
Sahil Bansal <bansalsahil740@gmail.com>
2021-03-22 15:59:07 +0530

    Problem 7-Let's Review @30DaysOfCode

---
+++ Day06.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int T= sc.nextInt();
+    
+    for(int i=0; i<=T-1; i++)
+    {
+        String S=sc.next();
+        for(int j=0; j< S.length();j++){
+            if(j%2==0){
+                System.out.print(S.charAt(j));
+            }
+            
+        }
+        System.out.print(" ");
+        for(int k=0;k<S.length();k++){
+            if(k%2!=0){
+                System.out.print(S.charAt(k));
+            }
+                }
+                            System.out.println("");
+
+    }
+        
+}
+}
+
