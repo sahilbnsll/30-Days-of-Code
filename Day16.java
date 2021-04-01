65853828970d24e59e8abaf39e02908419eb46a1
Sahil Bansal <bansalsahil740@gmail.com>
2021-04-01 11:19:53 +0530

    Problem 17-Exceptions- String to Integer @30DaysOfCode

---
+++ Day16.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Day16 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        try{
+            System.out.println(Integer.parseInt(S));
+        }
+        catch (Exception e){
+            System.out.println("Bad String");
+        }
+    }
+}
+
